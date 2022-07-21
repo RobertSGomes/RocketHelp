6c940aa686d0b14c7450602cb48799d227ab2502
RobertSGomes <robertsg.dev@gmail.com>
2022-07-21 09:07:16 -0300

    First commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+
+import { Envelope, Key } from "phosphor-react-native"
+
+import Logo from "../assets/logo_primary.svg";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+export function SignIn() {
+    const [mail, setMail] = useState("")
+    const [pass, setPass] = useState("")
+
+    const { colors } = useTheme()
+
+    return (
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
+
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta
+            </Heading>
+
+            <Input
+                placeholder="E-mail"
+                mb={4}
+                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+                onChangeText={setMail}
+            />
+            <Input
+                placeholder="Senha"
+                mb={8}
+                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+                onChangeText={setPass}
+                secureTextEntry
+            />
+
+            <Button title="Entrar" w="full" />
+        </VStack>
+    );
+}
